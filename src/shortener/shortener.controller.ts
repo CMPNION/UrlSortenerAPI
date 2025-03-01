e027f6c1bf9379bffa724ba211446e47b4f7ce12
CMPNION <norkindima57@gmail.com>
2025-03-01 19:09:16 +0500

    some

--- src/shortener/shortener.controller.ts
+++ src/shortener/shortener.controller.ts
@@ -1,7 +1,8 @@
 import { Send } from './../../node_modules/@types/express-serve-static-core/index.d';
 import { white } from './../../node_modules/@colors/colors/index.d';
-import { Controller, Get, Post, Body, Param, Redirect, HttpCode, HttpStatus, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Redirect, HttpCode, HttpStatus, UsePipes, ValidationPipe, Res } from '@nestjs/common';
 import { ShortenerService } from './shortener.service';
+import { Response } from 'express';
 import { ShortenerDto } from '../dto/shortener.dto';
 import { TokenGenerator, TokenBase } from 'ts-token-generator';
 
@@ -11,9 +12,9 @@ export class ShortenerController {
 
   @Post("/toshort")
   @UsePipes(new ValidationPipe({whitelist: true, forbidNonWhitelisted: true}))
-  async createShortUrl(@Body() body: ShortenerDto ) {
+  async createShortUrl(@Body() body: ShortenerDto, @Res() resp: Response ) {
     const res = await this.shortenerService.createShortUrl(body);
-    return res;
+    return resp.status(201).json({ shortUrl: res });
   }
 
   @Get(":token")
